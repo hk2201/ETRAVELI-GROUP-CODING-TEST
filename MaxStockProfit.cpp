9d75183d18c2d93870141416f6bc7394aa291c6a
Harshad <harshadkhandare9000@gmail.com>
2023-03-10 14:17:18 +0530

    3 Questions

--- MaxStockProfit.cpp
+++ MaxStockProfit.cpp
@@ -1,3 +1,36 @@
 #include <iostream>
-#include <algortihm>
+#include <algorithm>
+#include <vector>
 using namespace std;
+
+int maxProfit(vector<int>& stocks){
+    int maxx = 0;
+
+    for(int i = 0 ; i < stocks.size() ; i++){
+        int temp = 0;
+        for(int j = i+1; j < stocks.size() ; j++){
+            if(stocks[j] > stocks[i]){
+                temp = stocks[j] - stocks[i];
+            }
+        }
+        maxx = max(temp,maxx);
+        temp = 0;
+    }
+
+
+    return maxx;
+}
+
+
+int main(){
+    vector<int> stocks = {1,2,3,1,5,6};
+
+    int ans = maxProfit(stocks);
+
+    cout<<"The Maximum Profit obtained is "<<ans;
+
+
+
+
+    return 0;
+}
